f7d932fd70b2136f606058d46c5ad53944df0c2a
Jorge Espindola Filho <jlespindolaf@gmail.com>
2022-09-15 14:27:57 -0300

    Added API consume and created modal

---
+++ web/src/components/Header.tsx
@@ -0,0 +1,8 @@
+export function Header () {
+    return (
+        
+        <h1 className='text-6xl text-white font-black mt-20'>
+            Seu <span className='bg-nlw-gradient text-transparent bg-clip-text'>duo</span> está aqui.
+        </h1>
+    )
+}
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
     extend: {      
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
-        'nlw-gradient': 'linear-gradient(89.86deg, #9572fc 0%, #43e7ad 50%, #e1d55d 100cd ..%)',
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572fc 0%, #43e7ad 50%, #e1d55d 100%)',
         'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       }
     },
